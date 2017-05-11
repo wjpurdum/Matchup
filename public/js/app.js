bdcb4daa1689f014b43745a6a2ec9204cad3448a
Whitney <whitney.purdum@gmail.com>
2017-05-11 16:36:35 -0400

    update functions

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,47 @@
 angular
   .module("matchup", ["ui.router", "ngResource"])
   .config(["$stateProvider", RouterFunction])
+  .factory("teamFactory", [
+    "$resource",
+    TeamFactoryFunction
+  ])
+  .factory("LeagueFactory", [
+    "$resource",
+    LeagueFactoryFunction
+  ])
+  .controller("LeagueIndexController", [
+    "LeagueFactory",
+    LeagueIndexControllerFunction
+  ])
+  .controller("TeamIndexController", [
+    "TeamFactory",
+    TeamIndexControllerFunction
+  ])
+  .controller("TeamShowController", [
+    "TeamFactory",
+    TeamShowControllerFunction
+  ])
+
+  ])
+
+  // FUNCTIONS
+
+  function RouterFunction($stateProvider) {
+    $stateProvider
+    .state("LeagueIndexControllerFunction")
+
+  }
+
+
+
+  function TeamFactoryFunction() {
+
+  }
+
+  function LeagueFactoryFunction() {
+
+  }
+
+  function LeagueIndexControllerFunction() {
+
+  }
