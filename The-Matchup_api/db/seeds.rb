ceb4a5b267cc379d05a4c25b4480a55d19461c71
Whitney <whitney.purdum@gmail.com>
2017-05-11 14:18:24 -0400

    update seed data with example

--- The-Matchup_api/db/seeds.rb
+++ The-Matchup_api/db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+League.destroy_all
+Team.destroy_all
+
+premier_league = League.create({
+  name: "Premier League",
+  number_matches: 380,
+  number_teams: 20,
+  year: 2016
+  })
+
+efl_championchip =
+
+
+Team.create([
+  {
+  team_name: "Hull City FC",
+  crest_url: "http://upload.wikimedia.org/wikipedia/de/a/a9/Hull_City_AFC.svg",
+  league: premier_league,
+  fixtures: "http://api.football-data.org/v1/teams/322/fixtures",
+  self: "http://api.football-data.org/v1/teams/322",
+  players: "http://api.football-data.org/v1/teams/322/players"
+},
+
+
+
+
+
+
+  ])
