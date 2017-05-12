db834e79a4055942d7d1bef34c407bed7873dcb3
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-05-12 17:41:28 -0400

    set up ajax call to access features

--- public/js/app.js
+++ public/js/app.js
@@ -16,6 +16,8 @@ angular
     TeamFactoryFunction
   ])
   .controller("LeagueIndexController", [
+    "$scope",
+    "$http",
     "LeagueFactory",
     LeagueIndexControllerFunction
   ])
@@ -70,8 +72,23 @@ function TeamFactoryFunction($resource) {
   return $resource("http://locahost:3000/teams/:id")
 }
 
-function LeagueIndexControllerFunction(LeagueFactory) {
+function LeagueIndexControllerFunction($scope, $http, LeagueFactory) {
   this.leagues = LeagueFactory.query();
+
+  var url = "http://api.football-data.org/v1/competitions/426/fixtures";
+    $http.get(url).success( function(response) {
+       $scope.leagues = response;
+       let allFixtures = response.fixtures;
+       let teamOne = "Southampton FC";
+       let teamTwo = "Stoke City FC"
+       console.log(allFixtures)
+       // Loop through fixtures and print fixture that matches team names
+       for(var i = 0; i < allFixtures.length; i++){
+          if((teamOne == allFixtures[i].homeTeamName || teamOne == allFixtures[i].awayTeamName) && (teamTwo == allFixtures[i].homeTeamName || teamTwo == allFixtures[i].awayTeamName)){
+            console.log(allFixtures[i])
+          }
+       }
+    });
 }
 
 function TeamShowControllerFunction(){
