fcfb25515c58f01a7b8ffe5235cb8461ad6f5ff7
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-05-12 11:41:54 -0400

    set up leagues controller

--- The-Matchup_api/app/controllers/leagues_controller.rb
+++ The-Matchup_api/app/controllers/leagues_controller.rb
@@ -1,5 +1,13 @@
 class LeaguesController < ApplicationController
   def index
     @leagues = League.all
+
+    render json: @leagues
+  end
+
+  def show
+    @league = League.find(params[:id])
+
+    render json: @league
   end
 end
